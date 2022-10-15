981714afe52b670b956841e268c14afa3e392035
Mecca-A <meccaakello@gmail.com>
2022-10-14 21:57:38 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - prints in lower case
+ * Return: 0
+ */
+int main(void)
+{
+
+	char lcl;
+
+	for (lcl =  'a'; lcl <= 'z'; lcl++)
+	{
+
+		putchar(lcl);
+
+	}
+	putchar('\n');
+	return (0);
+
+}
