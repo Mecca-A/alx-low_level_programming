6b735fb6c73ca427469c85041803de42d534e36b
Mecca-A <meccaakello@gmail.com>
2022-10-16 17:26:39 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * main - Prints numbers between 00 to 89.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, x;
+
+	i = 48;
+	x = 48;
+
+	while (x < 58)
+	{
+		i = 48;
+		while (i < 58)
+		{
+			if (e != i && x < i)
+			{
+				putchar(x);
+				putchar(i);
+				if (i == 57 && x == 56)
+				{
+					break;
+				}
+				putchar(',');
+				putchar(' ');
+			}
+			i++;
+		}
+		x++;
+	}
+	putchar('\n');
+	return (0);
+
+}
+
