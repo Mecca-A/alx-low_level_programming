7b46a54e8c44ba3d11af222d6d4c386e0f4f5251
Mecca-A <meccaakello@gmail.com>
2022-10-18 12:25:50 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -3,6 +3,7 @@
 /**
  * times_table - print the 9 times table
  */
+
 void times_table(void)
 {
 
@@ -11,6 +12,7 @@ void times_table(void)
 	for (num = 0; num <= 9; num++)
 	{
 		_putchar('0');
+
 		for (mult = 1; mult <= 9; muli++)
 		{
 			_putchar(',');
