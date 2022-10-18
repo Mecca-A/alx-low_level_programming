2bc12233b9e7baf3712a33eccd49d58a12d22771
Mecca-A <meccaakello@gmail.com>
2022-10-18 13:44:54 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,19 +11,27 @@ void times_table(void)
 	for (num = 0; num <= 9; num++)
 	{
 		_putchar('0');
-
+		_putchar(',');
+		_putchar(' ');
 		for (mult = 1; mult <= 9; mult++)
 		{
-			_putchar(',');
-			_putchar(' ');
-
-			pro = num * mult;
-
-			if (prod <= 9)
-				_putchar(' ');
-			else
+			prod = (num * num);
+			if ((prod / 10) > 0)
+			{
 				_putchar((prod / 10) + '0');
+			}
+			else
+			{
+				_putchar(' ');
+			}
+			if (prod < 9)
+			{
+				_putchar(',');
+				_putchar('' ');
+			}
 		}
+		_putchar('\n');
+
 	}
 
 }
