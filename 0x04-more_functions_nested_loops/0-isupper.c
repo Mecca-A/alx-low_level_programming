0fa6a30d30ed8638843fb45d5a258752e26da7a6
Mecca-A <meccaakello@gmail.com>
2022-10-20 00:06:24 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isupper -> a function that checks upper case character
+ * @c: an integer paramer
+ * Return: anything but success
+ */
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
