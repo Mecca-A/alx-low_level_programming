8f6f8975f489e92f1f4cb2c23ab6726d27900202
Mecca-A <meccaakello@gmail.com>
2022-10-24 09:01:17 -0700

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,13 +3,13 @@
 /**
  * _strlen - returns the length of a string
  * @s: string
- * Return: length of the string
+ * Return: length
  */
 int _strlen(char *s)
 {
 	int len = 0;
 
-	while (*s != '\0'; s++)
+	while (*s != '\0')
 	{
 		len++;
 		s++;
