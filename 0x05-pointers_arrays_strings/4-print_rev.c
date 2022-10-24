058afa94cb9390007e8fd6ede2e99f6edb7c130e
Mecca-A <meccaakello@gmail.com>
2022-10-24 09:22:59 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_rev - Prints a string in reverse
+ * print_rev - prints a string in reverse
  * @s: string to be printed
  */
 void print_rev(char *s)
@@ -10,7 +10,6 @@ void print_rev(char *s)
 
 	while (s[i] != '\0')
 		i++;
-		len++;
 
 	for (i = i - 1; i >= 0; i--)
 		_putchar(s[i]);
