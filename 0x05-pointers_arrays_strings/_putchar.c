956950b7f902da7eef5a1dd084336bdf0d2db7dc
Mecca-A <meccaakello@gmail.com>
2022-10-24 07:25:17 -0700

    98 Battery st.

---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,14 @@
+
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: 1 (Success)
+ * On error, return -1, andset errno appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
