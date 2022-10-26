d7a508366c404bbf07edd716d1815a9cb2c2576f
Mecca-A <meccaakello@gmail.com>
2022-10-26 12:33:43 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -18,8 +18,8 @@ char *cap_string(char *str)
 				str[i - 1] == '\t' ||
 				str[i - 1] == '\n' ||
 				str[i - 1] == ',' ||
-				str[i - 1] == '.' ||
 				str[i - 1] == ';' ||
+				str[i - 1] == '.' ||
 				str[i - 1] == '!' ||
 				str[i - 1] == '?' ||
 				str[i - 1] == '"' ||
@@ -28,7 +28,7 @@ char *cap_string(char *str)
 				str[i - 1] == '{' ||
 				str[i - 1] == '}' ||
 				i == 0)
-			str -= 32;
+			str[i] -= 32;
 
 		i++;
 	}
