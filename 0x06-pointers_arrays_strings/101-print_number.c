8c446039d8b69c1796085ac4727808b86ed99cbd
Mecca-A <meccaakello@gmail.com>
2022-10-28 09:11:18 -0700

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,27 +7,27 @@
  */
 void print_number(int n)
 {
-	unsigned int x, y, count;
+	unsigned int m, d, count;
 
 	if (n < 0)
 	{
 		_putchar(45);
-		x = n * -1;
+		m = n * -1;
 	}
 	else
 	{
-		x = n;
+		m = n;
 	}
-	y = x;
+	d = m;
 	count = 1;
 
-	while (y > 9)
+	while (d > 9)
 	{
-		y / = 10;
+		d / = 10;
 		count *= 10;
 	}
 	for (; count >= 1; count /= 10)
 	{
-		_putchar(((x / count) % 10) + 48);
+		_putchar(((m / count) % 10) + 48);
 	}
 }
