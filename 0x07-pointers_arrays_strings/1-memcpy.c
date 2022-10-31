9e756f1736b9b445e5e994e6b02a42c087c9ac42
Mecca-A <meccaakello@gmail.com>
2022-10-31 15:00:35 -0700

    1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -2,11 +2,12 @@
 
 /**
  * _memcpy - copies @n bytes from the memory area pointed
- * to by the @src into the one pointed to by @dest
- * @src: source buffer to copy characters from
- * @dest: pointer to the memory area to copy @src into
- * @n: number of bytes to copy from @src
- * Return: a pointer to the destination buffer @dest
+ * to by the @src into the one pointed to by @dest.
+ * @dest: pointer to the memory area to copy @src into.
+ * @src: source buffer to copy characters from.
+ * @n: number of bytes to copy from @src.
+ *
+ * Return: a pointer to the destination buffer @dest.
  */
 void *_memcpy(void *dest, const void *src, size_t n)
 {
