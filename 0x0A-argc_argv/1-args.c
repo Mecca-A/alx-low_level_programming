f159720e51a686272ab025d2b90364da08361182
Mecca-A <meccaakello@gmail.com>
2022-11-04 14:03:37 -0700

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -4,7 +4,8 @@
 /**
  * main - Prints the number of arguments passed into main
  * @argc: Number of command line arguments
- * @argv: Array name Return: 0
+ * @argv: Array name
+ * Return: 0
  */
 
 int main(int argc, char *argv[])
