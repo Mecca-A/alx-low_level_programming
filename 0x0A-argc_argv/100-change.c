b14df984b0417c0e1b7e9c04ff6cb39fd7143125
Mecca-A <meccaakello@gmail.com>
2022-11-06 13:53:05 -0800

    100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,14 +1,17 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include <stdbool.h>
 
 /**
  * main - entry point
- * Description: prints the minimum number of coins to make change for an amoun
- * of money
- * @argc: amount of arguments passed through the program
- * @argv: pointer that contains the arrays of the arguments
+ * Description: prints the minimum number of coins to make change for an amount
+ * of money.
+ * @argc: amount of arguments passed through the program.
+ * @argv: pointer that contains the arrays of the arguments.
  * Return: the minimum number of coins needed to make the change for the amount
- * of money required
+ * of money required.
  */
 int main(int argc, char *argv[])
 {
